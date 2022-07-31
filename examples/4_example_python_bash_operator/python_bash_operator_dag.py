0926cab7dd00fea830594edf8bbd272f7ed53950
Jose Miguel Granda <jmgranda@>
2022-07-31 12:17:31 -0500

    Ejemplos DAGs

--- examples/4_example_python_bash_operator/python_bash_operator_dag.py
+++ examples/4_example_python_bash_operator/python_bash_operator_dag.py
@@ -29,7 +29,7 @@ dag = DAG(dag_id='DAG-Paralell_Python_Bash', description='Paralell DAG with Pyth
 # Step - 5
 tasks = [BashOperator(task_id='task_{0}'.format(t), bash_command='sleep 30'.format(t), dag=dag) for t in range(1, 5)]
 task_5 = PythonOperator(task_id='task_5', python_callable=process_data, op_args=['Read 1000 items'], dag=dag)
-task_6 = BashOperator(task_id='task_6', bash_command='echo "Pipeline Ok"', dag=dag)
+task_6 = BashOperator(task_id='task_6', bash_command='sleep 3', dag=dag)
 task_7 = BashOperator(task_id='task_7', bash_command='sleep 30', dag=dag)
 
 # Step - 6
